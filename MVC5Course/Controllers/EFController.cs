9ee15f163d75f01bd69eec9655d5948083f532cf
guuba <guuba1016@gmail.com>
2016-03-06 10:32:44 +0800

    新增MemberViewModel測試ViewModel及EFController測試於Product資料表中寫入資料

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        // GET: EF
+        public ActionResult Index()
+        {
+            var db = new FabricsEntities();
+
+            db.Product.Add(new Product()
+            {
+                ProductName = "BMW",
+                Price = 20,
+                Stock = 1,
+                Active = true
+            });
+            
+            db.SaveChanges();
+            var data = db.Product.ToList();
+
+            return View(data);
+        }
+    }
+}
---
+++ MVC5Course/Controllers/MemberProfileController.cs
@@ -0,0 +1,38 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MemberProfileController : Controller
+    {
+        // GET: MemberProfile
+        public ActionResult Index()
+        {
+
+            //var db = new FabricsEntities();
+            //var client = new Client() { ClientId=1};
+            //db.Client.Add(client);
+            //db.SaveChanges();
+    
+
+            return View();
+        }
+
+        
+        public ActionResult MemberProfile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MemberProfile(MemberViewModel data)
+        {
+            return View();
+        }
+    
+    }
+}
