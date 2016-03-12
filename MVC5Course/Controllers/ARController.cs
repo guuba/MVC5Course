c89a2a35d31595f89cfad7bad58343cd70c3e0c4
guuba <guuba1016@gmail.com>
2016-03-12 13:57:56 +0800

    25 練習實作 BaseController 共用控制器 ( 要設定 abstract 與自訂 HandleUnknownAction ) 26 練習不同的 ActionResult 用法 ( View, PartialView, Content, File )

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            //PartialView不會套用_layout
+            return PartialView("Index");
+        }
+
+        public ActionResult ContentTest()
+        {
+            //不建議這麼寫，更改需重新complier才可執行
+            return Content("<script>alert('Redriecting ...');</script>",
+                "application/javascript", Encoding.UTF8);
+        }
+
+        public ActionResult FileTest()
+        {
+            //ContentType對應名稱請參考 :http://tool.oschina.net/commons
+            
+            //直接在網頁上開啟
+            return File(
+               Server.MapPath("~/Content/alphago-logo.png"),
+               "image/png");
+            
+            ////加入第三個下載命名屬性，會直接下載並名命完成
+            //return File(
+            //    Server.MapPath("~/Content/alphago-logo.png"),
+            //    "image/png",
+            //    "GoGoGo.png");
+        }
+    }
+}
---
+++ MVC5Course/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        //若找不到Action則導回首頁
+        protected override void HandleUnknownAction(string actionName)
+        {
+            this.Redirect("/").ExecuteResult(this.ControllerContext);
+        }
+
+       protected ProductRepository repo = RepositoryHelper.GetProductRepository();
+
+        public ActionResult Debug()
+        {
+            return Content("DEBUG");
+        }
+    }
+}
